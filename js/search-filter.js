5a0b24b10767b29bd94b522863c0268ce39bc6fe
Mohamed Mostafa <m1mostafa>
2019-08-26 18:39:04 +0200

    last

---
+++ js/search-filter.js
@@ -0,0 +1,25 @@
+var snapSlider = document.getElementById('slider-snap');
+
+noUiSlider.create(snapSlider, {
+    start: [0, 500],
+    snap: true,
+    connect: true,
+    range: {
+        'min': 0,
+        '10%': 50,
+        '20%': 100,
+        '30%': 150,
+        '40%': 500,
+        '50%': 800,
+        'max': 1000
+    }
+});
+
+var snapValues = [
+    document.getElementById('slider-snap-value-lower'),
+    document.getElementById('slider-snap-value-upper')
+];
+
+snapSlider.noUiSlider.on('update', function (values, handle) {
+    snapValues[handle].innerHTML = values[handle];
+});
